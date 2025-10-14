e709fe3b5af061bd6e24fb73c7e1cda5dd8afa02
Stephen Foster <67705843+Stehfyn@users.noreply.github.com>
2025-10-13 21:35:53 -0700

    2st

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # ImGuiAppLayer
+
+```C++
+struct TestLayer : ImGuiAppLayer
+{
+  virtual void OnAttach(ImGuiApp*) override final
+  {
+    Counter++;
+  }
+
+  virtual void OnRender(const ImGuiApp* app) override final
+  {
+    ImGui::Begin("App Layer Window");
+    ImGui::Text("Hello from App Layer!");
+    ImGui::End();
+  }
+
+private:
+  int Counter;
+};
+
+static void ShowImGuiAppLayerDemo()
+{
+  static ImGuiApp app;
+
+  if (1 == ImGui::GetFrameCount())
+    ImGui::PushAppLayer<TestLayer>(&app);
+
+  ImGui::UpdateApp(&app);
+  ImGui::RenderApp(&app);
+}
+```
---
+++ imgui_applayer.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "imgui.h"
+
+#include <type_traits>
+
+struct ImGuiApp;
+struct ImGuiAppLayer;
+struct ImGuiAppLayerBase;
+
+struct ImGuiAppLayerBase
+{
+  virtual ~ImGuiAppLayerBase() = default;
+  virtual void OnAttach(ImGuiApp*) = 0;
+  virtual void OnDetach(ImGuiApp*) = 0;
+  virtual void OnUpdate(ImGuiApp*, float) = 0;
+  virtual void OnRender(const ImGuiApp*) = 0;
+
+protected:
+  ImGuiAppLayerBase() = default;
+};
+
+struct ImGuiAppLayer : ImGuiAppLayerBase 
+{
+  virtual void OnAttach(ImGuiApp*) override {}
+  virtual void OnDetach(ImGuiApp*) override {}
+  virtual void OnUpdate(ImGuiApp*, float) override {}
+  virtual void OnRender(const ImGuiApp*) override {}
+};
+
+struct ImGuiApp
+{
+  ImVector<ImGuiAppLayerBase*> Layers;
+};
+
+namespace ImGui
+{
+  template <typename T>
+  inline void PushAppLayer(ImGuiApp* app);
+  inline void PopAppLayer(ImGuiApp* app);
+  inline void UpdateApp(ImGuiApp* app);
+  inline void RenderApp(const ImGuiApp* app);
+}
+
+namespace ImGui
+{
+  template <typename T>
+  inline void PushAppLayer(ImGuiApp* app)
+  {
+    IM_STATIC_ASSERT((std::is_base_of_v<ImGuiAppLayerBase, T>));
+    IM_ASSERT(app);
+
+    app->Layers.push_back(IM_NEW(T)());
+    app->Layers.back()->OnAttach(app);
+  }
+
+  inline void PopAppLayer(ImGuiApp* app)
+  {
+    IM_ASSERT(app);
+
+    if (app->Layers.empty())
+      return;
+
+    app->Layers.back()->OnDetach(app);
+    app->Layers.pop_back();
+  }
+
+  inline void UpdateApp(ImGuiApp* app)
+  {
+    IM_ASSERT(app);
+
+    for (auto& layer : app->Layers)
+      layer->OnUpdate(app, GetIO().DeltaTime);
+  }
+
+  inline void RenderApp(const ImGuiApp* app)
+  {
+    IM_ASSERT(app);
+
+    for (auto& layer : app->Layers)
+      layer->OnRender(app);
+  }
+}
+
