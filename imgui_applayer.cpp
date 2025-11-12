c80f0cda15daee259546747c96c40503d84e2b50
Stephen Foster <stephenfostercpp@gmail.com>
2025-11-11 23:16:03 -0800

    Expanded on data dependency demo
    Signed-off-by: Stephen Foster <stephenfostercpp@gmail.com>

--- imgui_applayer.cpp
+++ imgui_applayer.cpp
@@ -1,6 +1,7 @@
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include "imgui_applayer.h"
 #include "imgui_internal.h"
+#include <ctime>
 
 void ImGuiApp::OnExecuteCommand(ImGuiAppCommand cmd)
 {
@@ -88,12 +89,71 @@ void ImGuiAppStatusLayer::OnRender(const ImGuiApp* app) const
 
 namespace
 {
+  // Encourage "pure" design, such that a control is "agnostic to the data passing through it."
   static void RenderTextT(const char* text, ImVec2 text_size, ImVec2 pos, ImVec2 avail, float t_value)
   {
     float offset = avail.x * 0.5f + (t_value * 0.5f * (avail.x - text_size.x));
     ImGui::SetCursorScreenPos(pos + ImVec2(offset, 0.0f) + (text_size * -0.5f));
     ImGui::Text("%s", text);
   }
+
+  struct RandomTimeData
+  {
+    char label[128];
+    char type[128];
+    float max_timer_secs;
+  };
+
+  struct RandomTimeTempData
+  {
+    bool generate;
+  };
+
+  struct RandomTimeControlDemo : ImGuiControl <RandomTimeData, RandomTimeTempData>
+  {
+    // Random time between 1 and 30 seconds
+    static float GenerateTime() { return static_cast<float>(1 + (rand() % 30)); }
+
+    virtual void OnInitialize(ImGuiApp*, RandomTimeData* data) const override final
+    {
+      std::string_view sv;
+
+      srand(static_cast<unsigned int>(time(nullptr)));
+
+      sv = ImGuiType<decltype(this)>::Name;
+
+      ImStrncpy(data->type, sv.data(), sv.length());
+      ImFormatString(data->label, sizeof(data->label), "%s", data->type);
+
+      data->max_timer_secs = GenerateTime();
+    }
+
+    virtual void OnUpdate(float dt, RandomTimeData* data, const RandomTimeTempData* temp_data, const RandomTimeTempData* last_temp_data) const override final
+    {
+      IM_UNUSED(temp_data);
+      IM_UNUSED(last_temp_data);
+
+      if (temp_data->generate)
+        data->max_timer_secs = GenerateTime();
+    }
+
+    virtual void OnRender(const RandomTimeData* data, RandomTimeTempData* temp_data) const override final
+    {
+      IM_UNUSED(data);
+      IM_UNUSED(temp_data);
+
+      if (ImGui::Begin(data->label))
+      {
+        ImGui::Text("%s", "Max Timer Seconds");
+
+        temp_data->generate = ImGui::Button("Generate");
+        ImGui::SameLine();
+
+        ImGui::Text("%.1f", data->max_timer_secs);
+      }
+      ImGui::End();
+    }
+  };
 
   struct BreathingControlData
   {
@@ -112,23 +172,28 @@ namespace
     bool hovered;
   };
 
-  struct BreathingControlDemo : ImGuiControl<BreathingControlData, BreathingControlTempData>
+  struct BreathingControlDemo : ImGuiControl<BreathingControlData, BreathingControlTempData, RandomTimeData>
   {
-    virtual void OnInitialize(ImGuiApp*, BreathingControlData* data) const override final
+    virtual void OnInitialize(ImGuiApp*, BreathingControlData* data, const RandomTimeData* random_time_data) const override final
     {
-      std::string_view sv = ImGuiType<decltype(this)>::Name;
+      std::string_view sv;
+
+      IM_UNUSED(random_time_data); // Explicitly ignore dependency on initialization (as we explicitly use it on OnUpdate())
+
+      sv = ImGuiType<decltype(this)>::Name;
 
       ImStrncpy(data->type, sv.data(), sv.length());
       ImFormatString(data->label, sizeof(data->label), "%s", data->type);
     }
 
-    virtual void OnUpdate(float dt, BreathingControlData* data, const BreathingControlTempData* temp_data, const BreathingControlTempData* last_temp_data) const override final
+    virtual void OnUpdate(float dt, BreathingControlData* data, const BreathingControlTempData* temp_data, const BreathingControlTempData* last_temp_data, const RandomTimeData* random_time_data) const override final
     {
       data->timer_secs = ImMax(0.0f, data->timer_secs - dt);
 
       if (temp_data->hovered ^ last_temp_data->hovered)
       {
-        data->timer_secs = 30.0f * temp_data->hovered;
+        // If hovered, use RandomTimeData::max_timer_secs to set the timer (otherwise, data->timer_secs will be set to 0.0f)
+        data->timer_secs = temp_data->hovered * random_time_data->max_timer_secs;
         data->t_value = 0.0f;
         data->t_direction = 1.0f;
       }
@@ -150,12 +215,12 @@ namespace
       }
     }
 
-    virtual void OnRender(const BreathingControlData* data, BreathingControlTempData* temp_data) const override final
+    virtual void OnRender(const BreathingControlData* data, BreathingControlTempData* temp_data, const RandomTimeData* random_time_data) const override final
     {
+      IM_UNUSED(random_time_data); // Explicitly ignore dependency on rendering (as we explicitly use it on OnUpdate())
+
       if (ImGui::Begin(data->label))
       {
-        ImRect r;
-        ImVec2 pos;
         ImVec2 size = ImVec2(ImGui::GetContentRegionAvail().x, 2.0f * ImGui::GetFrameHeight());
 
         ImGui::PushStyleColor(ImGuiCol_ButtonHovered, data->col);
@@ -163,9 +228,9 @@ namespace
         temp_data->hovered = ImGui::IsItemHovered();
         ImGui::PopStyleColor();
 
-        pos = ImGui::GetCursorScreenPos();
+        ImVec2 pos = ImGui::GetCursorScreenPos();
         size = ImGui::GetContentRegionAvail();
-        r = { pos, pos + size };
+        ImRect r = { pos, pos + size };
 
         ImGui::PushClipRect(r.Min, r.Max, true);
         ImGui::NewLine();
@@ -223,7 +288,13 @@ namespace ImGui
       {
         InitializeApp(&app);
 
-        PushAppControl<BreathingControlDemo>(&app);
+        // This demo showcases a button whose color appears to "breathe" while hovered,
+        // (i.e. BreathingControlDemo) with a timer that is randomly set each time the
+        // button is hovered, which is managed by another control (RandomTimeControlDemo).
+        {
+          PushAppControl<RandomTimeControlDemo>(&app);
+          PushAppControl<BreathingControlDemo>(&app);
+        }
       }
 
       UpdateApp(&app);
